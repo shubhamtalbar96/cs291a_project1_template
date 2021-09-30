f6848d16a84ee5c9181241701741c5e819930b8b
Shubham Talbar <91352665+shubhamtalbar96@users.noreply.github.com>
2021-09-29 20:50:23 -0700

    Added json parse

--- function.rb
+++ function.rb
@@ -73,7 +73,7 @@ def main(event:, context:)
       return response(body: event, status: 422)
     else
       payload = {
-        data: event["body"],
+        data: JSON.parse(event["body"]),
         exp: Time.now.to_i + 5,
         nbf: Time.now.to_i + 2
       }
